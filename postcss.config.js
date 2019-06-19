2deb2d74458b14b2ab2db99919062d98c168d1b7
Nick Saunders <nick@saunde.rs>
2019-06-19 10:37:17 -0700

    Minify CSS.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("postcss-font-magician")
+    require("postcss-font-magician"),
+    require("cssnano")({ preset: "default" })
   ]
 };
