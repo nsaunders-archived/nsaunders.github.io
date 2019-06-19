f2fa5c1b4f2a71facf1a98b30a694abe9cebcaf0
Nick Saunders <nick@saunde.rs>
2019-06-19 10:44:16 -0700

    Purge unused CSS.

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("postcss-font-magician"),
+    require("@fullhuman/postcss-purgecss")({ content: ["index.html"] }),
     require("cssnano")({ preset: "default" })
   ]
 };
