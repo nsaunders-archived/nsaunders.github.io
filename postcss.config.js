8b95a24c96b7fa4f3a3a6eec8e5c256ea85a78b0
Nick Saunders <nick@saunde.rs>
2019-06-20 08:11:22 -0700

    Optimize for mobile displays.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV !== "production") {
+  console.warn("Currently using development configuration. For production builds, set NODE_ENV to \"production\".");
+}
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("postcss-font-magician"),
-    require("@fullhuman/postcss-purgecss")({ content: ["index.html"] }),
-    require("cssnano")({ preset: "default" })
-  ]
+    NODE_ENV === "production" ? require("@fullhuman/postcss-purgecss")({ content: ["index.html"] }) : null,
+    NODE_ENV === "production" ? require("cssnano")({ preset: "default" }) : null
+  ].filter(a => a)
 };
