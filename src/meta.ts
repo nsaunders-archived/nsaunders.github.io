75c302d5734d9d01d545ba7de657df9f0cecbb68
Nick Saunders <nick@saunde.rs>
2023-07-30 12:29:30 -0700

    Drop CNAME and config changes for GitHub Pages.

--- src/meta.ts
+++ src/meta.ts
@@ -1,5 +1,7 @@
 export const title = "nsaunders.dev";
 export const description = "Nick Saunders' technical blog and profile";
-export const publicURL = "https://nsaunders.dev";
+export const publicURL = process.env.VERCEL
+  ? "https://nsaunders.dev"
+  : "https://nsaunders.github.io";
 export const posts = `${publicURL}/posts`;
 export const rss = `${publicURL}/rss.xml`;
